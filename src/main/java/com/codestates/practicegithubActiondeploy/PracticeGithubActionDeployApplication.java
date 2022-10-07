f2ab2155268f854eb1761c8539272bb221b9cbf6
JanuaryKim <unool6976@gmail.com>
2022-10-07 16:07:39 +0900

    dddd

--- src/main/java/com/codestates/practicegithubActiondeploy/PracticeGithubActionDeployApplication.java
+++ src/main/java/com/codestates/practicegithubActiondeploy/PracticeGithubActionDeployApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PracticeGithubActionDeployApplication {
- 
+
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeGithubActionDeployApplication.class, args);
 	}
